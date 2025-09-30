5c429213fda870cd770fa3513cf52134f8035ca9
Musiki8213 <sithomolamusiki@gmail.com>
2025-09-30 12:02:42 +0200

    created the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+
+import './App.css';
+import { WordSearch } from './components/WordSearch';
+import { DefinitionDisplay } from './components/DefinitionDisplay';
 
 function App() {
-  
   return (
     <>
-      
+      <h1>Dictionary</h1>
+      <WordSearch />
+      <hr />
+      <DefinitionDisplay />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
